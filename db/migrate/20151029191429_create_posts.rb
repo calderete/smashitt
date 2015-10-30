0e8459f4747e2bba7563b6d0f7161b8cde70dd9a
Harrison Andrews <harrisonandrews1986@gmail.com>
2015-10-30 09:33:52 -0400

    updated the controllers for post and comment, as well as added our routes.

--- db/migrate/20151029191429_create_posts.rb
+++ db/migrate/20151029191429_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
       t.integer :user_id
-      t.text :content
+      t.text :url #this was changed from :content originally.
       t.text :title
       t.timestamps null: false
     end
