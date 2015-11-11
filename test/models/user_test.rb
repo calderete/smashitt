b5b86425aa3f029cac9e9b5172beb6b278b976cb
Charles <codprestige10@gmail.com>
2015-11-10 19:12:12 -0500

    got a test to work

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
+require 'pry'
 
 class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def test_user
+  	user = User.new
+  	assert user.valid?
+
+  end
 end
