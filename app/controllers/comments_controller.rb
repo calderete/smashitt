82e854299bde796ac089dad6147df3692caa447b
Charles <codprestige10@gmail.com>
2015-11-19 17:23:50 -0500

    removed some binding.prys....never push this stuff...headaches...nightmares

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,6 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:id])
     @post.comments.create!(content: params[:comment])
-    binding.pry
     redirect_to post_show_path(@post)
   end
 
