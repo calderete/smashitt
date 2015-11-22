04ca74576d612f9e6c1a4c63e48229af52c92cdf
Charles <codprestige10@gmail.com>
2015-11-22 18:38:22 -0500

    everything is broken

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,20 @@
 Rails.application.routes.draw do
 
-devise_for :users
+#devise_for :users, controllers: {sessions: "users/sessions" }
 
-#  sessions: "users/sessions" 
-#}
 
-#  devise_for :users, path: "auth", path_names: { 
-#    sign_in: 'login',
-#    sign_out: 'logout',
-#    password: 'secret',
-#    confirmation: 'verification',
-#    unlock: 'unblock',
-#    registration: 'register',
-#    sign_up: 'cmon_let_me_in'
-#           }
+  #devise_for :users, path: "auth", path_names: 
+  #  sign_in: 'login',
+  #  sign_out: 'logout',
+  #  password: 'secret',
+  #  confirmation: 'verification',
+  #  unlock: 'unblock',
+  #  registration: 'register',
+  #  sign_up: 'cmon_let_me_in'
+           
   
 
-  root to: "welcome#index"
+  root to: "devise/registrations#new"
 
 #regestrations routes
 #  get "signup", to: "regestrations#new"
